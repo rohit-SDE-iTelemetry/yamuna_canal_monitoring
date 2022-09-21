552793d13810ad9c8027fdbc92991ad6eec6eae0
aashish <aashishsingh1999@gmail.com>
2022-09-21 11:44:39 +0530

    changes dashboard abd reports ui

--- yamuna_canal_monitoring/monitoring_app/urls.py
+++ yamuna_canal_monitoring/monitoring_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from monitoring_app.views import dashboard
+from monitoring_app.views import dashboard,reports_dash
 
 urlpatterns = [
-    path('dashboard', dashboard),
+    path('dashboard', dashboard,name = "dashboard"),
+    path('reports_dash', reports_dash,name = "reports_dash"),
+
 ]
