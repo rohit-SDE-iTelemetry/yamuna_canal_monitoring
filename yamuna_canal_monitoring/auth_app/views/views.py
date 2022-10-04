218a98e5eeed3cfd577e36abeb60cc59507b784d
rohit-SDE-iTelemetry <rohit.kumar@i-telemetry.com>
2022-10-04 17:39:51 +0530

    code updated

--- yamuna_canal_monitoring/auth_app/views/views.py
+++ yamuna_canal_monitoring/auth_app/views/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
+from django.contrib.auth import authenticate,login,logout
+# from django.contrib.messages import constants as messages
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from auth_app.models import UserProfile
 
 
-# login page
-def login(request):
+# login view
+def signin(request):
     if request.method == 'GET':
         context = {}
-        return render(request,'auth_screens/login.html')
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        else:
+            return render(request,'auth_screens/login.html')
 
     if request.method == 'POST':
-        return render(request,'auth_screens/login.html')
+        email = request.POST.get('email')
+        password =request.POST.get('password')
+        user = authenticate(request,username=email,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            context = {}
+            context['error_msg'] = 'Invalid Username or Password'
+            return render(request,'auth_screens/login.html',context)
 
+        # return render(request,'auth_screens/login.html')
 
+# logout view
+def logout_view(request):
+    logout(request)
+    return redirect('/')
